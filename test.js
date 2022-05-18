234b14ffc8f015f96a0f61f5888626f8dfccde55
David Konsumer <konsumer@jetboystudio.com>
2022-05-18 01:31:46 -0700

    use doc for gh pages

--- test.js
+++ test.js
@@ -5,22 +5,22 @@ import tiled from './index.js'
 
 describe('tiled-load', () => {
   test('loadMap, inline tilesets (readFile)', async () => {
-    expect(await tiled('demo_inline.tmj', './doc/')).toMatchSnapshot()
+    expect(await tiled('demo_inline.tmj', './docs/')).toMatchSnapshot()
   })
 
   test('loadMap, external tilesets (readFile)', async () => {
-    expect(await tiled('demo.tmj', './doc/')).toMatchSnapshot()
+    expect(await tiled('demo.tmj', './docs/')).toMatchSnapshot()
   })
 
   test('loadMap, inline tilesets (fetch)', async () => {
     globalThis.fetch = jest.fn(async () => ({ async text () { return '{}' } }))
-    expect(await tiled('demo_inline.tmj', './doc/')).toMatchSnapshot()
+    expect(await tiled('demo_inline.tmj', './docs/')).toMatchSnapshot()
     expect(globalThis.fetch).toHaveBeenCalledTimes(1)
   })
 
   test('loadMap, external tilesets (fetch)', async () => {
     globalThis.fetch = jest.fn(async () => ({ async text () { return '{}' } }))
-    expect(await tiled('demo.tmj', './doc/')).toMatchSnapshot()
+    expect(await tiled('demo.tmj', './docs/')).toMatchSnapshot()
     expect(globalThis.fetch).toHaveBeenCalledTimes(1)
   })
 })
